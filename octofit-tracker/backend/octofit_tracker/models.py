31e73c9dd94a4c53c1128dc47c8903658cc6140a
genai-praveenmali <praveen.reddy.mali@accenture.com>
2025-12-29 11:30:19 +0000

    test_data creation

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from djongo import models as djongo_models
+from bson import ObjectId
 
 class Team(models.Model):
+    id = djongo_models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 class User(models.Model):
+    id = djongo_models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
@@ -15,6 +19,7 @@ class User(models.Model):
         return self.name
 
 class Activity(models.Model):
+    id = djongo_models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=100)
     duration = models.IntegerField()  # in minutes
@@ -24,6 +29,7 @@ class Activity(models.Model):
         return f"{self.user.name} - {self.type}"
 
 class Workout(models.Model):
+    id = djongo_models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.ManyToManyField(User, related_name='workouts', blank=True)
@@ -32,6 +38,7 @@ class Workout(models.Model):
         return self.name
 
 class Leaderboard(models.Model):
+    id = djongo_models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.IntegerField()
 
